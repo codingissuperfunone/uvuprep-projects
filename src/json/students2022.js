58a00f0967dd7b6653ff29a310522ebdb899f0ba
JABJAB <107072340+codingissuperfunone@users.noreply.github.com>
2022-07-14 10:49:05 -0600

    Update students2022.js

--- src/json/students2022.js
+++ src/json/students2022.js
@@ -4,6 +4,11 @@ const students = {
     "https://replit.com/@buckldav/DinoGameP5Starter",
     "https://replit.com/@buckldav/PizzaStarter",
   ],
+  "Jared B": [
+    "https://replit.com/@JABJAB1/CS-final-project-Hunt-the-Wumpus#script.js",
+    "https://replit.com/@JABJAB1/DandD-fun#index.html", 
+    "https://replit.com/@JABJAB1/multi-page-project#index.html",
+  ],
 };
 
 const studentsSorted = Object.keys(students)
